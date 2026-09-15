b53525773559e190909130da1a3b9029093c1253
Kuzemskyi-Maksym <maksymkuzemskyi@gmail.com>
2026-09-15 15:51:44 +0300

    Fix vercel routing and wsgi entrypoint

--- project/config/wsgi.py
+++ project/config/wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
-app = application
+
+app = application  # <-- ОБОВ'ЯЗКОВО ДЛЯ VERCEL
